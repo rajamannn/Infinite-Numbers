a3660bdae4ce93c259b9dd15be20873fc51234b3
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2025-02-19 23:00:27 +0530

    Add files via upload

---
+++ Infinte_loop.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Infinte_loop
+{
+        public static void main(String[] args)
+        {
+            //int n = 5;for (int p = 0; p <= n; p++)
+            {
+                int k = 1;
+                while (k <= 5) {
+                    System.out.print("Hello ");
+                    k--;
+                }
+                System.out.println();
+            }
+        }
+    }
